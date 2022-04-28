9e39f53a183c67cf9ecdf423f31c6072492ecb80
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-04-28 15:24:09 +0100

    react hooks checkpoint

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovieCard = () => {
+  return (
+    <div>
+      <h3>Movie Card Component</h3>
+    </div>
+  );
+};
+
+export default MovieCard;
---
+++ src/Components/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovieList = () => {
+  return (
+    <div>
+      <h3>Movie List Component</h3>
+    </div>
+  );
+};
+
+export default MovieList;
